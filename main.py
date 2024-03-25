b7f29f58c3eb9862189ff08b9617a0f12e5ed97c
takunnma <sennmonnkakidori@gmail.com>
2024-03-25 09:27:05 +0900

    あさっぱらからなにやってんのおれ、システム自体は数字関連以外は完成、すうじどうしよ

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 import discord
+import pykakasi
+from pyokaka import okaka
 
-TOKEN = 'THi5IsDuMMyaCCesSTOK3n00.Cl2FMQ.ThIsi5DUMMyAcc3s5ToKen0000'
+TOKEN = open('token', 'r').read()
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content=True
+
+client = discord.Client(intents=intents)
+
+AA = r"""
+   _____                _ _   _                                   _           
+  / ____|              (_) | (_)                                 | |          
+ | (___   ___ _ __  ___ _| |_ ___   _____  __      _____  _ __ __| |          
+  \___ \ / _ \ '_ \/ __| | __| \ \ / / _ \ \ \ /\ / / _ \| '__/ _` |          
+  ____) |  __/ | | \__ \ | |_| |\ V /  __/  \ V  V / (_) | | | (_| |          
+ |_____/ \___|_| |_|___/_|\__|_| \_/ \___|   \_/\_/ \___/|_|_ \__,_|          
+     | |    | |          | | (_)                           | |                
+   __| | ___| |_ ___  ___| |_ _  ___  _ __    ___ _   _ ___| |_ ___ _ __ ___  
+  / _` |/ _ \ __/ _ \/ __| __| |/ _ \| '_ \  / __| | | / __| __/ _ \ '_ ` _ \ 
+ | (_| |  __/ ||  __/ (__| |_| | (_) | | | | \__ \ |_| \__ \ ||  __/ | | | | |
+  \__,_|\___|\__\___|\___|\__|_|\___/|_| |_| |___/\__, |___/\__\___|_| |_| |_|
+                                                   __/ |                      
+                                                  |___/                       
+"""
+
+skiplist = ["\n", "、", "。", " ", "　"]
+
+print(AA)
+
+words = open('words.txt', 'r').read().split("\n")
+
+def tohira(text):
+    kakasi = pykakasi.kakasi()
+    return "".join([i["hira"] for i in kakasi.convert(okaka.convert(text))])
+
 
 @client.event
 async def on_ready():
-    print('ログインしました')
+    print("discordに接続しました")
 
 @client.event
 async def on_message(message):
+    msg = message.content
+    msg = tohira(msg)
+    for i in range(len(skiplist)):
+        msg = msg.replace(skiplist[i],"")
+    
     if message.author.bot:
         return
     
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    if any([msg.find(i) != -1 for i in words]):
+        hitlist = [i for i in words if msg.find(i) != -1]
+        hitcontent = "".join([f"`{i}`、" for i in hitlist])[0:-1]
+        rpmsg = f"あなたのメッセージには破廉恥な言葉、{hitcontent}が含まれていますわ"
+        await message.reply(rpmsg)
 
 client.run(TOKEN)
